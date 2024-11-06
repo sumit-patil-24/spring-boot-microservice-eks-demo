9829001805d2925da161373b5848c13ec264a2a6
Anmol Vishwakarma <anmol.vns2000@gmail.com>
2024-11-06 14:50:21 +0530

    Fix: Add CORS configuration in User and Employee services to support cross-origin requests

---
+++ employee-service/src/main/java/com/example/employee_service/config/ServiceCorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.employee_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class ServiceCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
