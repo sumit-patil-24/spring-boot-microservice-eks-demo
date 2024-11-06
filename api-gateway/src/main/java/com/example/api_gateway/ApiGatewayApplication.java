d2724b6068006233560f443d91e0116da8788a55
Anmol Vishwakarma <anmol.vns2000@gmail.com>
2024-11-06 14:42:35 +0530

    Fix: Fixed user and employee service URLs in application.yaml to point to localhost:8081 and localhost:8082 respectively, and added route locator for user and employee services in API Gateway

--- api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -1,13 +1,35 @@
 package com.example.api_gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder, @Value("${user.service.url}") String userServiceUrl,
+									 @Value("${employee.service.url}") String employeeServiceUrl) {
+		return builder
+				.routes()
+				// Route to user service APIs
+				.route(r -> r.path("/user-service/v3/api-docs").and().method(HttpMethod.GET).uri(userServiceUrl))
+				.route(r -> r.path("/api/v1/user/**").and().method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+						.uri(userServiceUrl))
+
+				// Route to employee service APIs
+				.route(r -> r.path("/employee-service/v3/api-docs").and().method(HttpMethod.GET).uri(employeeServiceUrl))
+				.route(r -> r.path("/api/v1/employee/**").and().method(HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE)
+						.uri(employeeServiceUrl))
+				.build();
 	}
 
 }
